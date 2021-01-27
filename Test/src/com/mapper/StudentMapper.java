d1e031c61ef54fead3291cd059f612505a247f51
Administrator <Administrator@DESKTOP-5UN5MRQ>
2021-01-27 22:47:13 +0800

    kdfdlkdlfdsl

--- Test/src/com/mapper/StudentMapper.java
+++ Test/src/com/mapper/StudentMapper.java
@@ -2,4 +2,8 @@ package com.mapper;
 
 public class StudentMapper {
 
+	
+	public static void main(String[] args) {
+		System.out.println("this is StudentMappers");
+	}
 }
--- Test/src/com/model/Student.java
+++ Test/src/com/model/Student.java
@@ -2,4 +2,11 @@ package com.model;
 
 public class Student {
 
+	private int id;
+	
+	private String name;
+	
+	private String haahh;
+	
+	private int i;
 }
