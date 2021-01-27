694a5624daa7546340367a4e4ca2953d58f6522e
adminhuyh <35656551+adminhuyh@users.noreply.github.com>
2021-01-27 23:00:00 +0800

    Update Student.java

--- Test/src/com/model/Student.java
+++ Test/src/com/model/Student.java
@@ -6,7 +6,14 @@ public class Student {
 	
 	private String name;
 	
-	private String haahh;
+	
+	private String name1;
+	
+	private String name2;
+	
+	private String name3;
+	
+	private String haahh4;
 	
 	private int i;
 }
