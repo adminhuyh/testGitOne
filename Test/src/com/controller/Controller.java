bc2a6ee9a84dad19387adf24544618c65deb0af9
adminhuyh <35656551+adminhuyh@users.noreply.github.com>
2021-01-27 23:29:12 +0800

    Update Controller.java

--- Test/src/com/controller/Controller.java
+++ Test/src/com/controller/Controller.java
@@ -5,10 +5,10 @@ public class Controller {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("dfjfdafajlk");
+		System.out.println("dfjfdafadddddddddddddsjlk");
 
 
-
+ 
 
 		System.out.println("dfjfdafajlk");
 		System.out.println("dfjfdafajlk");
